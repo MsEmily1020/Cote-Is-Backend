709ef37cbe4f3f85bab13429bee74d93e9fbc0e4
hyeg0121 <hyeg0121@naver.com>
2023-06-04 15:22:52 +0900

    [fix] 게시글 작성 API 구현
    엔티티 구성, 레포지터리 만들기, 서비스 메서드 코드 작성, 컨트롤러 메서드
    코드 작성

---
+++ api/src/main/java/org/coteis/domain/Article.java
@@ -0,0 +1,64 @@
+package org.coteis.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_no", updatable = false)
+    private Integer articleNo;
+
+    @Column(name = "title", nullable = false)   // 제목
+    private String title;
+
+    @Column(name = "author", nullable = false)  // 작성자
+    private String author;
+
+    @Column(name = "date", nullable = false)  // 게시 날짜
+    private String date;
+
+    @Column(name = "test_explain", nullable = false)    // 문제 설명
+    private String testExplain;
+
+    @Column(name = "answer", nullable = false)  // 코드 해답
+    private String answer;
+
+    @Column(name = "input_example") // 입력 예
+    private String inputExample;
+
+    @Column(name = "output_example")    // 출력 예
+    private String outputExample;
+
+    @Column(name = "speed") // 최적화,속도
+    private String speed;
+
+    @Column(name = "code_explain")  // 코드 설명
+    private String codeExplain;
+
+    @Column(name = "concept")   // 개념 정리
+    private String concept;
+
+    @Builder
+    public Article(String title, String author, String date, String testExplain, String answer, String inputExample, String outputExample, String speed, String codeExplain, String concept) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.testExplain = testExplain;
+        this.answer = answer;
+        this.inputExample = inputExample;
+        this.outputExample = outputExample;
+        this.speed = speed;
+        this.codeExplain = codeExplain;
+        this.concept = concept;
+    }
+}
