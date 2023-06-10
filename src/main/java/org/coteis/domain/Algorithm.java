59fbf40a7135e202087f4cb94422339cb4104a26
hyeg <hyeg0121@naver.com>
2023-06-10 13:38:42 +0900

    [feat] Builder 생성자 생성

--- src/main/java/org/coteis/domain/Algorithm.java
+++ src/main/java/org/coteis/domain/Algorithm.java
@@ -2,6 +2,7 @@ package org.coteis.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +18,8 @@ public class Algorithm {
     @Column(name = "algorithm_name", nullable = false)  // 알고리즘 종류
     private String algorithmName;
 
+    @Builder
+    public Algorithm(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
 }
