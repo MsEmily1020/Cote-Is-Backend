a61f28590fef05ed0364f128ba9df55429f4b4cd
MsEmily1020 <peace10200khs@gmail.com>
2023-06-17 12:07:30 +0900

    [Feat] AddUserRequest 객체를 인수로 받는 회원 정보 추가 메서드 생성

--- src/main/java/org/coteis/dto/user/AddUserRequest.java
+++ src/main/java/org/coteis/dto/user/AddUserRequest.java
@@ -3,16 +3,23 @@ package org.coteis.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.coteis.domain.user.User;
+import org.coteis.repository.user.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class AddUserRequest {
     private String userName;
     private String userId;
     private String userPw;
     private String userEmail;
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User toEntity(){
         return User.builder()
@@ -23,5 +30,13 @@ public class AddUserRequest {
                 .build();
 
     }
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .userId(dto.getUserId())
+                .userPw(bCryptPasswordEncoder.encode(dto.getUserPw()))
+                .build()).getUserNo();
+    }
+
 }
 
