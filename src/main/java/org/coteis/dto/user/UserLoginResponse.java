d31893191d00a581f9f4fa1078b9b667742c154c
MsEmily1020 <peace10200khs@gmail.com>
2023-11-05 19:59:23 +0900

    [Feat] login 시 user response dto 생성

--- src/main/java/org/coteis/dto/user/UserLoginResponse.java
+++ src/main/java/org/coteis/dto/user/UserLoginResponse.java
@@ -4,18 +4,14 @@ import lombok.Getter;
 import org.coteis.domain.user.User;
 
 @Getter
-public class UserResponse {
+public class UserLoginResponse {
     private final Long userNo;
-    private final String userName;
     private final String userId;
     private final String userPw;
-    private final String userEmail;
 
-    public UserResponse(User user){
+    public UserLoginResponse(User user){
         this.userNo = user.getUserNo();
-        this.userName = user.getUserName();
         this.userId = user.getUserId();
         this.userPw = user.getUserPw();
-        this.userEmail = user.getUserEmail();
     }
 }
