d4c2f8c6d193536edf255a9c4e22cdd851237b11
MsEmily1020 <peace10200khs@gmail.com>
2023-11-06 09:02:53 +0900

    [Feat]  user 로그인 기능 구현

--- src/main/java/org/coteis/dto/user/UserLoginRequest.java
+++ src/main/java/org/coteis/dto/user/UserLoginRequest.java
@@ -3,6 +3,7 @@ package org.coteis.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.coteis.domain.user.User;
 
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/org/coteis/service/user/UserService.java
+++ src/main/java/org/coteis/service/user/UserService.java
@@ -5,8 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.coteis.domain.user.User;
 import org.coteis.dto.user.AddUserRequest;
 import org.coteis.dto.user.UpdateUserRequest;
+import org.coteis.dto.user.UserLoginRequest;
+import org.coteis.dto.user.UserResponse;
 import org.coteis.repository.user.UserRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,30 +19,30 @@ public class UserService {
     private final UserRepository userRepository;
 
     // create
-    public User save(AddUserRequest request){ return userRepository.save(request.toEntity()); }
+    public User save(AddUserRequest request) { return userRepository.save(request.toEntity()); }
 
     // read
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public List<User> findAll() { return userRepository.findAll(); }
+
+    public User findLoginUser(UserLoginRequest request) {
+        return userRepository.findByUserIdAndUserPw(request.getUserId(), request.getUserPw());
     }
 
     // delete
-    public void delete(Long id) {
-        userRepository.deleteById(id);
-    }
+    public void delete(Long id) { userRepository.deleteById(id); }
 
     // update
     @Transactional
     public User update(Long id, UpdateUserRequest request){
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
         user.update(
                 request.getUserName(),
                 request.getUserId(),
                 request.getUserPw(),
                 request.getUserEmail()
         );
-
         return user;
     }
 }
