2c4bfc082ccd2908081eac189e9fa95ad44bbef1
MsEmily1020 <peace10200khs@gmail.com>
2023-12-11 12:20:03 +0900

    [Docs] API 명세서 작성 4.특정 글 정보 업데이트

--- src/test/java/org/coteis/controller/article/ArticleControllerTest.java
+++ src/test/java/org/coteis/controller/article/ArticleControllerTest.java
@@ -7,7 +7,9 @@ import org.coteis.domain.category.Language;
 import org.coteis.domain.category.Previoustest;
 import org.coteis.domain.user.User;
 import org.coteis.dto.article.AddArticleRequest;
+import org.coteis.dto.article.UpdateArticleRequest;
 import org.coteis.dto.user.AddUserRequest;
+import org.coteis.dto.user.UpdateUserRequest;
 import org.coteis.repository.article.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,8 +24,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -203,7 +204,7 @@ class ArticleControllerTest {
                                 preprocessRequest(prettyPrint()),
                                 preprocessResponse(prettyPrint()),
                                 pathParameters( // path 파라미터 정보 입력
-                                        parameterWithName("id").description("유저 번호 pk")
+                                        parameterWithName("id").description("글 번호 pk")
                                 ),
                                 responseFields( // response 필드 정보 입력
                                         fieldWithPath("articleNo").description("글 번호 pk"),
@@ -240,7 +241,93 @@ class ArticleControllerTest {
     }
 
     @Test
-    void updateArticle() {
+    @DisplayName("updateArticle() : 특정 글의 정보를 업데이트")
+    void updateArticle() throws Exception {
+        UpdateArticleRequest request = new UpdateArticleRequest("title",
+                "testExplain",
+                "answser",
+                "inputExample",
+                "outputExample",
+                "speed",
+                "codeExplain",
+                "concept",
+                new Algorithm("algorithmName"),
+                new Difficulty("difficultyName"),
+                new Language("langaugeName"),
+                new Previoustest("previoustestName"));
+
+        mockMvc.perform(
+                        put("/api/articles/{id}", 1L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo( // rest docs 문서 작성 시작
+                        document("article-put", // 문서 조각 디렉토리 명
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                pathParameters( // path 파라미터 정보 입력
+                                        parameterWithName("id").description("글 번호 pk")
+                                ),
+
+                                requestFields(
+                                        fieldWithPath("title").description("글 제목"),
+                                        fieldWithPath("testExplain").description("문제 설명"),
+                                        fieldWithPath("answer").description("코드 정답"),
+                                        fieldWithPath("inputExample").description("입력 예시"),
+                                        fieldWithPath("outputExample").description("출력 예시"),
+                                        fieldWithPath("speed").description("최적화, 속도"),
+                                        fieldWithPath("codeExplain").description("코드 설명"),
+                                        fieldWithPath("concept").description("개념 정리"),
+                                        fieldWithPath("algorithmNo").description("카테고리 알고리즘 번호 pk"),
+                                        fieldWithPath("algorithmNo.algorithmNo").description("카테고리 알고리즘 번호 pk").ignored(),
+                                        fieldWithPath("algorithmNo.algorithmName").description("카테고리 알고리즘 이름").ignored(),
+                                        fieldWithPath("difficultyNo").description("카테고리 난이도 번호 pk"),
+                                        fieldWithPath("difficultyNo.difficultyNo").description("카테고리 난이도 번호 pk").ignored(),
+                                        fieldWithPath("difficultyNo.difficultyName").description("카테고리 난이도 이름").ignored(),
+                                        fieldWithPath("languageNo").description("카테고리 언어 번호 pk"),
+                                        fieldWithPath("languageNo.languageNo").description("카테고리 언어 번호 pk").ignored(),
+                                        fieldWithPath("languageNo.languageName").description("카테고리 언어 이름").ignored(),
+                                        fieldWithPath("previoustestNo").description("카테고리 기출 문제 번호 pk"),
+                                        fieldWithPath("previoustestNo.previoustestNo").description("카테고리 기출 문제 번호 pk").ignored(),
+                                        fieldWithPath("previoustestNo.previoustestName").description("카테고리 기출 문제 이름").ignored()
+                                ),
+
+                                responseFields( // response 필드 정보 입력
+                                        fieldWithPath("articleNo").description("글 번호 pk"),
+                                        fieldWithPath("title").description("글 제목"),
+                                        fieldWithPath("createdDate").description("생성 일자"),
+                                        fieldWithPath("modifiedDate").description("수정 일자"),
+                                        fieldWithPath("testExplain").description("문제 설명"),
+                                        fieldWithPath("answer").description("코드 정답"),
+                                        fieldWithPath("inputExample").description("입력 예시"),
+                                        fieldWithPath("outputExample").description("출력 예시"),
+                                        fieldWithPath("speed").description("최적화, 속도"),
+                                        fieldWithPath("codeExplain").description("코드 설명"),
+                                        fieldWithPath("concept").description("개념 정리"),
+                                        fieldWithPath("userNo").description("유저 번호 pk"),
+                                        fieldWithPath("userNo.userNo").description("유저 번호 pk").ignored(),
+                                        fieldWithPath("userNo.userName").description("유저 이름").ignored(),
+                                        fieldWithPath("userNo.userId").description("유저 아이디").ignored(),
+                                        fieldWithPath("userNo.userPw").description("유저 패스워드").ignored(),
+                                        fieldWithPath("userNo.userEmail").description("유저 이메일").ignored(),
+                                        fieldWithPath("algorithmNo").description("카테고리 알고리즘 번호 pk"),
+                                        fieldWithPath("algorithmNo.algorithmNo").description("카테고리 알고리즘 번호 pk").ignored(),
+                                        fieldWithPath("algorithmNo.algorithmName").description("카테고리 알고리즘 이름").ignored(),
+                                        fieldWithPath("difficultyNo").description("카테고리 난이도 번호 pk"),
+                                        fieldWithPath("difficultyNo.difficultyNo").description("카테고리 난이도 번호 pk").ignored(),
+                                        fieldWithPath("difficultyNo.difficultyName").description("카테고리 난이도 이름").ignored(),
+                                        fieldWithPath("languageNo").description("카테고리 언어 번호 pk"),
+                                        fieldWithPath("languageNo.languageNo").description("카테고리 언어 번호 pk").ignored(),
+                                        fieldWithPath("languageNo.languageName").description("카테고리 언어 이름").ignored(),
+                                        fieldWithPath("previoustestNo").description("카테고리 기출 문제 번호 pk"),
+                                        fieldWithPath("previoustestNo.previoustestNo").description("카테고리 기출 문제 번호 pk").ignored(),
+                                        fieldWithPath("previoustestNo.previoustestName").description("카테고리 기출 문제 이름").ignored()
+                                )
+                        )
+                )
+        ;
     }
 
     @Test
